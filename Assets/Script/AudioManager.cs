6252f4302a0f8db832bbf648bf9cae9d1f579972
2355010002@kientruchanoi.edu.vn <2355010002@kientruchanoi.edu.vn>
2026-09-24 21:21:10 +0700

    cập nhật 9:21

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -30,16 +30,19 @@ void GhiNhoTranh(PaintingData tranh)
 }
 
     public void PhatThuyetMinh()
+{
+    Debug.Log("Đang xử lý tranh: " + tranhHienTai.tenTranh + 
+               " | File audio: " + (tranhHienTai.amThanhThuyetMinh != null ? tranhHienTai.amThanhThuyetMinh.name : "KHÔNG CÓ"));
+
+    if (tranhHienTai.amThanhThuyetMinh == null)
     {
-        if (tranhHienTai.amThanhThuyetMinh == null)
-        {
-            Debug.Log("Tranh"+ tranhHienTai.tenTranh +"chưa có file âm thanh.");
-            return;
-        }
-        audioSource.Stop();
-        audioSource.clip = tranhHienTai.amThanhThuyetMinh;
-        audioSource.Play();
+        Debug.Log("Tranh"+ tranhHienTai.tenTranh +"chưa có file âm thanh.");
+        return;
     }
+    audioSource.Stop();
+    audioSource.clip = tranhHienTai.amThanhThuyetMinh;
+    audioSource.Play();
+}
     public void DungPhat()
 {
     audioSource.Stop();
